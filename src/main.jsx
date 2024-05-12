fe7eb4945e7e97f77a004cb5acbdb82a04032713
sadiqur057 <sadiqur057@gmail.com>
2024-05-13 00:32:08 +0600

    Used queryclient provider from tanstack query

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,21 @@ import './index.css'
 import AuthProvider from './providers/AuthProvider'
 import { RouterProvider } from 'react-router-dom';
 import Routes from './routes/Routes';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <HelmetProvider>
-        <RouterProvider router={Routes}></RouterProvider>
-      </HelmetProvider>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <HelmetProvider>
+          <RouterProvider router={Routes}></RouterProvider>
+        </HelmetProvider>
+      </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
