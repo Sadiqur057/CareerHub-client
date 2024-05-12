8d6e28de1f3a5a9cb3ed42b76ba4ef84e9ecfbe2
sadiqur057 <sadiqur057@gmail.com>
2024-05-12 22:58:56 +0600

    created auth provider

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -11,6 +11,7 @@ import {
   updateProfile,
 } from "firebase/auth";
 import auth from "../firebase/firebase.config";
+import axios from "axios";
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
@@ -52,12 +53,27 @@ const AuthProvider = ({ children }) => {
   };
   useEffect(() => {
     const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      const userEmail = currentUser?.email || user?.email
+      const loggedUser = { email: userEmail }
       setLoading(false);
       setUser(currentUser);
       setReload(false)
+      if (currentUser) {
+        axios.post('http://localhost:5000/jwt', loggedUser, { withCredentials: true })
+          .then(res => {
+            console.log("token response", res.data)
+          })
+      } else {
+        axios.post('http://localhost:5000/logout', loggedUser, {
+          withCredentials: true
+        })
+          .then(res => {
+            console.log(res.data)
+          })
+      }
     });
     return () => unSubscribe();
-  }, [reload]);
+  }, [reload, user?.email]);
 
   const AuthInfo = {
     user,
