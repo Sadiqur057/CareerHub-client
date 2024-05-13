90c51465c2fc7a0c4d563d7a489a6dac82f2f888
sadiqur057 <sadiqur057@gmail.com>
2024-05-13 22:17:04 +0600

    handled auth change on reloading

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -2,6 +2,7 @@ import PropTypes from "prop-types";
 import { Navigate, useLocation } from "react-router-dom";
 import { Spinner } from "@material-tailwind/react";
 import useAuth from "../hooks/useAuth";
+import Swal from "sweetalert2";
 
 const PrivateRoutes = ({ children }) => {
   const location = useLocation();
@@ -15,8 +16,18 @@ const PrivateRoutes = ({ children }) => {
   }
   if (user) {
     return children;
+  } else {
+    Swal.fire({
+      title: "Login to proceed",
+      text: "Please login to continue exploring various service of our website",
+      icon: "warning",
+      confirmButtonColor: "#3085d6",
+      confirmButtonText: "Login now"
+    })
+    return <Navigate to="/login" state={location?.pathname}></Navigate>;
+
+
   }
-  return <Navigate to="/login" state={location?.pathname}></Navigate>;
 };
 
 PrivateRoutes.propTypes = {
