7a34f8e6cffe8191650e7fff52710571c479a597
sadiqur057 <sadiqur057@gmail.com>
2024-05-14 18:58:56 +0600

    added dynamic title

--- src/pages/allJobs/AllJobs.jsx
+++ src/pages/allJobs/AllJobs.jsx
@@ -1,10 +1,10 @@
-import { Link, useLoaderData } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { TbListDetails } from "react-icons/tb";
 import axios from "axios";
 import { useState } from "react";
-import { Option, Select, Spinner } from "@material-tailwind/react";
-import { TfiLayoutColumn3Alt, TfiLayoutColumn2Alt } from "react-icons/tfi";
+import { Spinner } from "@material-tailwind/react";
 import { useQuery } from "@tanstack/react-query";
+import { Helmet } from "react-helmet-async";
 
 
 
@@ -12,12 +12,10 @@ const AllJobs = () => {
 
   const [displayData, setDisplayData] = useState([])
 
-  // const allJobs = useLoaderData()
-
   const {isPending, data: allJobs } = useQuery({
     queryKey: ['all-jobs'],
     queryFn: async ()=>{
-      const response = await axios.get(`http://localhost:5000/all-jobs`)
+      const response = await axios.get(`https://career-hub-server-one.vercel.app/all-jobs`)
       setDisplayData(response.data)
       return response.data
     }
@@ -25,20 +23,18 @@ const AllJobs = () => {
   if (isPending) {
     return (
       <div className="min-h-[calc(100vh-80px)] w-full flex justify-center items-center">
-        <Spinner className="h-12 w-12" color="teal" />
+        <Spinner className="h-12 w-12" color="orange" />
       </div>
     );
   }
 
   const handleSearch = (e) => {
-    console.log("clicked")
     e.preventDefault();
     const keyword = e.target.keyword.value;
-    console.log("searched", keyword)
     if(!keyword){
       return setDisplayData(allJobs)
     }
-    axios.get(`http://localhost:5000/search/${keyword}`)
+    axios.get(`https://career-hub-server-one.vercel.app/search/${keyword}`)
       .then(data => {
         setDisplayData(data.data)
       })
@@ -47,9 +43,11 @@ const AllJobs = () => {
 
   return (
     <div className="w-[90%] mx-auto">
-
-      <form onSubmit={handleSearch}  className="flex justify-end gap-2 my-6">
-        <input type="text" placeholder="Search" className="border-c-primary border outline-c-primary  px-4 rounded-xl" name="keyword" />
+      <Helmet>
+        <title>CH | All Jobs</title>
+      </Helmet>
+      <form onSubmit={handleSearch}  className="flex justify-end gap-2 my-6 ">
+        <input type="text" placeholder="Search" className="border-c-primary border outline-c-primary  px-4 rounded-xl w-[180px] md:w-[230px]" name="keyword" />
         <input type="submit" className="py-2 px-4 bg-c-primary rounded-lg text-white font-medium" value="submit" />
       </form>
       <div className="mb-10 overflow-auto">
