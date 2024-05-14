0abf0dbd1585c6654712e3e217fd0288dccb48b7
sadiqur057 <sadiqur057@gmail.com>
2024-05-14 18:57:04 +0600

    updated alignment

--- src/components/Jobs/JobByCategoryCard.jsx
+++ src/components/Jobs/JobByCategoryCard.jsx
@@ -1,5 +1,5 @@
 // const axios = require('axios');
-
+import PropTypes from 'prop-types';
 import { CiCalendarDate } from "react-icons/ci";
 import { Link } from "react-router-dom";
 import { IoPersonSharp } from "react-icons/io5";
@@ -11,15 +11,12 @@ const JobByCategoryCard = ({ job }) => {
   return (
     <div className="flex">
       <div className="w-full">
-        <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col gap-3">
-          <div >
+        <div className="flex rounded-lg h-full bg-base-200 p-8 flex-col gap-3">
+          <div>
             <div className="flex gap-6 mb-3 md:mb-2 justify-between md:justify-start">
-              <h2 className="text-gray-900 text-xl font-semibold">{job_title}</h2>
-              <div className="px-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0 py-1 self-center">
-                active
-              </div>
+              <h2 className="text-2xl font-semibold">{job_title}</h2>
             </div>
-            <div className="flex flex-wrap items-center justify-between gap-2">
+            <div className="flex flex-wrap items-center justify-between gap-2 md:mb-4">
               <p>Posted by <span className="font-medium">{posted_by}</span></p>
               <p className="leading-relaxed text-base flex items-center gap-2"><CiCalendarDate className="text-2xl"></CiCalendarDate>{posting_date}</p>
             </div>
@@ -32,7 +29,7 @@ const JobByCategoryCard = ({ job }) => {
             </div>
 
             <div className="flex flex-wrap gap-3 justify-between items-center">
-              <p className="md:text-lg font-medium">Application deadline: <span className="font-medium">{deadline}</span></p>
+              <p className="md:text-lg nav">Application deadline: <span className="font-medium">{deadline}</span></p>
               <div className="flex justify-end w-full">
                 <Link to={`/job-details/${_id}`}>
                   <button className="text-white bg-c-primary hover:bg-c-hover px-4 py-2 rounded-xl">View Details
@@ -45,6 +42,10 @@ const JobByCategoryCard = ({ job }) => {
       </div>
     </div>
   );
+};
+
+JobByCategoryCard.propTypes = {
+  job: PropTypes.object
 };
 
 export default JobByCategoryCard;
--- src/components/Jobs/JobsByCategoryContainer.jsx
+++ src/components/Jobs/JobsByCategoryContainer.jsx
@@ -1,10 +1,16 @@
+import PropTypes from 'prop-types';
 const JobsCategoryContainer = ({children}) => {
   
   return (
-    <div className="mt-5 grid grid-cols-1 gap-3 lg:gap-4 md:grid-cols-2 ">
+    <div className="mt-6 md:mt-10 grid grid-cols-1 gap-3 lg:gap-4 md:grid-cols-2 ">
       {children}
     </div>
   );
+  
 };
+JobsCategoryContainer.propTypes = {
+  children: PropTypes.node
+};
+
 
 export default JobsCategoryContainer;
