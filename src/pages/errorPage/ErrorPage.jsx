ed8c7805f6ac4d239de2290461f2e20bee9ea406
sadiqur057 <sadiqur057@gmail.com>
2024-05-14 19:00:21 +0600

    added suitable gif on error

--- src/pages/errorPage/ErrorPage.jsx
+++ src/pages/errorPage/ErrorPage.jsx
@@ -1,20 +1,18 @@
-import { Link, useRouteError } from "react-router-dom";
-
+import { Link } from "react-router-dom";
+import errorImg from '../../assets/images/404.png'
+import { Helmet } from "react-helmet-async";
 const ErrorPage = () => {
-  const error = useRouteError();
-  const errorStatus = error.status || 404
-  const errorText = error?.statusText || "Not Found";
   return (
-<div className="min-h-screen flex flex-grow items-center justify-center bg-gray-50 p-4 md:p-10">
-  <div className="rounded-lg bg-white p-8 text-center shadow-xl max-w-[550px]">
-    <h1 className="mb-4 text-4xl font-bold text-[#272222]"> Error: {errorStatus}</h1>
-    <p className="text-gray-800 text-xl mb-3">This Page is  {errorText}
-    </p>
-    <p></p>
-    <p className="text-gray-600">Oops! The content you are looking for could not be found. </p>
-    <Link to="/" className="mt-4 btn bg-c-primary text-white hover:bg-c-hover border-none"> Back to Home</Link>
-  </div>
-</div>
+    <div className="min-h-screen flex flex-grow items-center justify-center bg-gray-50 p-4 md:p-10">
+      <Helmet>
+        <title>CH | Not Found</title>
+      </Helmet>
+      <div className="rounded-lg  p-8 text-center max-w-[650px] px-10">
+        <img src={errorImg} className=" h-80 w-80 md:w-[400px] md:h-[400px] mx-auto mb-6" alt="" />
+
+        <Link to="/" className="mt-4 btn bg-c-primary text-white hover:bg-c-hover border-none"> Back to Home</Link>
+      </div>
+    </div>
   );
 };
 
