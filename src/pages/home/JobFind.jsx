db327b98bf010a4f60432d4ab8bed88a7a668565
sadiqur057 <sadiqur057@gmail.com>
2024-05-15 14:08:15 +0600

    adjusted banner image

--- src/pages/home/JobFind.jsx
+++ src/pages/home/JobFind.jsx
@@ -1,3 +1,6 @@
+import img1 from "../../assets/images/img-1.jpg"
+import img2 from "../../assets/images/img-2.jpg"
+import img3 from "../../assets/images/img-3.jpg"
 export const JobFind = () => {
   return (
     <div className="w-[90%] pb-6 mx-auto md:px-24 lg:px-8 mb-10">
@@ -48,11 +51,11 @@ export const JobFind = () => {
           </div>
           <div className="max-w-xl mb-6">
             <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight  sm:text-4xl sm:leading-none">
-              Let us handle
+              Let us handle{' '}
               <br className="hidden md:block" />
               your next{' '}
               <span className="inline-block text-deep-purple-accent-400">
-                destination
+                Desired Job
               </span>
             </h2>
             <p className="text-base text-gray-700 md:text-lg">
@@ -65,19 +68,19 @@ export const JobFind = () => {
           <div className="flex flex-col items-end px-3">
             <img
               className="object-cover mb-6 rounded shadow-lg h-28 sm:h-48 xl:h-56 w-28 sm:w-48 xl:w-56"
-              src="https://images.pexels.com/photos/3184287/pexels-photo-3184287.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+              src={img1}
               alt=""
             />
             <img
               className="object-cover w-20 h-20 rounded shadow-lg sm:h-32 xl:h-40 sm:w-32 xl:w-40"
-              src="https://images.pexels.com/photos/3182812/pexels-photo-3182812.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+              src={img3}
               alt=""
             />
           </div>
           <div className="px-3">
             <img
               className="object-cover w-40 h-40 rounded shadow-lg sm:h-64 xl:h-80 sm:w-64 xl:w-80"
-              src="https://images.pexels.com/photos/3182739/pexels-photo-3182739.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500"
+              src={img2}
               alt=""
             />
           </div>
